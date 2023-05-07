871b5a172d1abf262e1599d9a8d9fd11c0ade0c2
Marvin Elsen <development@marvin-elsen.com>
2023-05-07 17:53:55 +0800

    Add zsh and wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.font = wezterm.font 'Iosevka rnepi'
+config.color_scheme = 'Sonokai (Gogh)'
+config.colors = {
+    cursor_fg = 'black',
+}
+
+config.window_padding = { left = 4, right = 4, top = 4, bottom = 4,
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.default_prog = { '/usr/bin/zsh' }
+config.skip_close_confirmation_for_processes_named = {
+  'bash',
+  'sh',
+  'zsh',
+  'fish',
+  'tmux',
+  'nu',
+  'cmd.exe',
+  'pwsh.exe',
+  'powershell.exe',
+}
+
+return config
